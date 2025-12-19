c8743a296072bc6d5c459a0e4b835108aba5b077
Wei Chen <134401401+Chin-Way@users.noreply.github.com>
2025-12-19 14:23:37 -0500

    Add files via upload
    The program is designed to be a simple MPG (Miles Per Gallons) calculator.

---
+++ csc_111_project_1_MPG_calculator.py
@@ -0,0 +1,4 @@
+miles=float(input("How many mailes have you driven? ")) #ask user number of miles
+gallons=float(input("How many gallons have you  used? ")) #ask user number of gallons used
+MPG=miles/gallons #calculating MPG
+print(f"Your car's MPG is {MPG}.") #print out MPG
